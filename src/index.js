78ea5ab06b29fd0710395196b22a3820b360f211
David Arevalo <davidarevaloc20@gmail.com>
2021-05-12 11:48:05 -0500

    finish the logic

--- src/index.js
+++ src/index.js
@@ -51,15 +51,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
         };
       }
       let day = dayReal();
-      document.getElementById(`mainDay${i}`).innerHTML=`
-      <div>
-        <h2>${day}</h2>
-        <p class="weather-Value-day">${grades}<span>ºC</span></p>
-        <p class="weather-type-day">${type}</p>
-        <p class="weather-Value">Min-temp: ${min}<span>ºC</span></p>
-        <p class="weather-Value">Max-temp: ${max}<span>ºC</span></p>
-      </div>
-      `
+      daysInfo(i, day, grades, type, min, max);
     };
       //catch
     } catch (error) {
@@ -100,8 +92,16 @@ function AirPressure(a) {
   `
 }
 // days with information
-function daysInfo(params) {
-  
+function daysInfo(i, day, grades, type, min, max) {
+  document.getElementById(`mainDay${i}`).innerHTML=`
+      <div>
+        <h2>${day}</h2>
+        <p class="weather-Value-day">${grades}<span>ºC</span></p>
+        <p class="weather-type-day">${type}</p>
+        <p class="weather-Value">Min-temp: ${min}<span>ºC</span></p>
+        <p class="weather-Value">Max-temp: ${max}<span>ºC</span></p>
+      </div>
+      `
 }
 
 //search-nav-menu button close
@@ -115,29 +115,10 @@ document.getElementById("faTimesCircle").addEventListener("click",()=>{
       const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+document.getElementById("searchNavInput").value+'&appid=7daaca17f73b8537056bf2d813d49c18')
       const data = await res.json()
       console.log(data)
-      console.log(data.name)
-      console.log(data.weather.main)
       //tranform Kelvin to centigrade
       let tranformationKelvin= (data.main.temp - 273.15) 
       //background gradient
-      function backgroundGradient(a){
-        if(a < 0){
-          document.getElementById("header").style.background="linear-gradient(to right, #4286f4, #373B44)";
-        } else if(a<10){
-          document.getElementById("header").style.background="linear-gradient(to right, #6dd5ed, #2193b0)";
-        } else if (a<20){
-          document.getElementById("header").style.background="linear-gradient(to right, #6dd5ed, #2193b0)";
-        } else if(a<30){
-          document.getElementById("header").style.background="linear-gradient(to right, #ffd194, #70e1f5)";
-        } else if (a<40){
-          document.getElementById("header").style.background="linear-gradient(to right, #EDDE5D, #F09819)";
-        } else if(a<50){
-          document.getElementById("header").style.background="linear-gradient(to right, #F9D423, #FF4E50)";
-        } else if(a<60){
-          document.getElementById("header").style.background="linear-gradient(to right, #F09819, #FF512F)";
-        }
-      }
-      console.log(backgroundGradient(Math.floor(tranformationKelvin)))
+      backgroundGradient(Math.floor(tranformationKelvin))
       //Print in the Dom
       document.getElementById("mainWeather").innerHTML=`
       <h1>${data.name}</h1>
@@ -147,14 +128,62 @@ document.getElementById("faTimesCircle").addEventListener("click",()=>{
       <p>Min-Temp: ${Math.floor(data.main.temp_min- 273.15)}<span>ºC</span></p>
       <p>Max-Temp: ${Math.floor(data.main.temp_max- 273.15)}<span>ºC</span></p>
       `
+      //Information of weather Days
+      InformationDAy(data.coord.lat, data.coord.lon) 
+      //modules weather
+      SpeedWidt(data.wind.speed); 
+      Humidity(data.main.humidity);
+      Visibility(data.visibility);
+      AirPressure(data.main.pressure)
       // Date and Hours
       var date = new Date();
-      onsole.log(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+      console.log(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
     } catch (error) {
       console.log(error)
     }
   }
-
+//share other api for information for days
+function InformationDAy(latitude, longitude) {
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=7daaca17f73b8537056bf2d813d49c18`)
+  .then(resp => resp.json())
+  .then(data => {
+    for (let i = 0; i < 8; i++) {
+      let grades = Math.floor(data.daily[i].temp.day-273.15);
+      let type = data.daily[i].weather[0].main;
+      let min = Math.floor(data.daily[i].temp.min-273.15);
+      let max = Math.floor(data.daily[i].temp.max-273.15);
+      function dayReal(){
+        if(days[i] !== days[f.getDay()]){
+          return days[i]
+        } else {
+          return "Today"
+        };
+      }
+      let day = dayReal();
+      daysInfo(i, day, grades, type, min, max);
+    };
+    
+  })
+  .catch(err=> console.log(err))
+}
+//background gradient
+function backgroundGradient(a){
+  if(a < 0){
+    document.getElementById("header").style.background="linear-gradient(to right, #4286f4, #373B44)";
+  } else if(a<10){
+    document.getElementById("header").style.background="linear-gradient(to right, #6dd5ed, #2193b0)";
+  } else if (a<20){
+    document.getElementById("header").style.background="linear-gradient(to right, #6dd5ed, #2193b0)";
+  } else if(a<30){
+    document.getElementById("header").style.background="linear-gradient(to right, #ffd194, #70e1f5)";
+  } else if (a<40){
+    document.getElementById("header").style.background="linear-gradient(to right, #EDDE5D, #F09819)";
+  } else if(a<50){
+    document.getElementById("header").style.background="linear-gradient(to right, #F9D423, #FF4E50)";
+  } else if(a<60){
+    document.getElementById("header").style.background="linear-gradient(to right, #F09819, #FF512F)";
+  }
+}
 //search country
 document.getElementById("search-nav-button").addEventListener("click", ()=>{
   fetchData();
